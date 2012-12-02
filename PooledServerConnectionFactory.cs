6607e6d4da5b29d3d4072adaa34228ee7964478b
Alan Hatter <alan.hatter@gmail.com>
2012-12-01 18:44:03 -0600

    Checking in

---
+++ PooledServerConnectionFactory.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.DirectoryServices.Protocols;
+using System.Linq;
+using System.Net;
+using LinqToLdap;
+
+namespace ServerPoolConnectionFactory
+{
+
+    public class PooledServerConnectionFactory : ILdapConnectionFactory
+    {
+        private readonly ConcurrentDictionary<string, ServerPoolMemberConnectionFactory> _servers;
+
+        public PooledServerConnectionFactory()
+        {
+            _servers = new ConcurrentDictionary<string, ServerPoolMemberConnectionFactory>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public void AddServer(LdapDirectoryIdentifier identifier, int maxConnections, int protocolVersion = 3, bool ssl = false, double? timeout = null, NetworkCredential credentials = null, AuthType? authType = null)
+        {
+            var serverName = identifier.Servers[0];
+            var factory = new LdapConnectionFactory(serverName);
+            if (credentials != null)
+                factory.AuthenticateAs(credentials);
+            if (authType.HasValue)
+                factory.AuthenticateBy(authType.Value);
+
+            if (timeout.HasValue)
+                factory.ConnectionTimeoutIn(timeout.Value);
+
+            factory.ProtocolVersion(protocolVersion);
+
+            if (identifier.FullyQualifiedDnsHostName)
+                factory.ServerNameIsFullyQualified();
+
+            if (identifier.Connectionless)
+                factory.UseUdp();
+
+            if (ssl) factory.UseSsl();
+
+            factory.UsePort(identifier.PortNumber);
+
+            _servers[serverName] = new ServerPoolMemberConnectionFactory(serverName, factory, maxConnections); 
+        }
+
+        public void RemoveServer(string serverName)
+        {
+            ServerPoolMemberConnectionFactory factory;
+            if (!_servers.TryRemove(serverName, out factory))
+            {
+                throw new InvalidOperationException(serverName + " not found.");
+            }
+        }
+
+        public LdapConnection GetConnection()
+        {
+            var factory = _servers.FirstOrDefault(c => c.Value.CanConnect);
+
+            if (Equals(factory, default(KeyValuePair<string, ServerPoolMemberConnectionFactory>))) 
+                throw new InvalidOperationException("No connections available");
+
+            return factory.Value.GetConnection();
+        }
+
+        public void ReleaseConnection(LdapConnection connection)
+        {
+            var server = ((LdapDirectoryIdentifier)connection.Directory).Servers[0];
+
+            _servers[server].ReleaseConnection(connection);
+        }
+
+        private class ServerPoolMemberConnectionFactory : ILdapConnectionFactory
+        {
+            private ILdapConnectionFactory _factory;
+            private int _maxConnections;
+            private int _connectionCount;
+            private string _serverName;
+
+            public ServerPoolMemberConnectionFactory(string serverName, ILdapConnectionFactory factory, int maxConnections)
+            {
+                _serverName = serverName;
+                _factory = factory;
+                _maxConnections = maxConnections;
+            }
+
+            public bool CanConnect { get { return _maxConnections > _connectionCount; } }
+
+            public LdapConnection GetConnection()
+            {
+                if (!CanConnect) throw new InvalidOperationException("Too many connections for " + _serverName);
+                
+                var connection = _factory.GetConnection();
+                _connectionCount++;
+
+                return connection;
+            }
+
+            public void ReleaseConnection(LdapConnection connection)
+            {
+                _factory.ReleaseConnection(connection);
+                _connectionCount--;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.Protocols;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToLdap;
+using SharpTestsEx;
+
+namespace ServerPoolConnectionFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new LdapConfiguration();
+
+            var factory = new PooledServerConnectionFactory();
+            factory.AddServer(new LdapDirectoryIdentifier("localhost", 389), 1);
+            factory.AddServer(new LdapDirectoryIdentifier("computername", 389), 1);
+
+            config.ConfigureCustomFactory(factory);
+
+            var connection1 = config.ConnectionFactory.GetConnection();
+            connection1.GetServerName().Satisfy(s => s.Equals("localhost") || s.Equals("computername"));
+
+            var connection2 = config.ConnectionFactory.GetConnection();
+            connection2.GetServerName().Satisfy(s => s.Equals("localhost") || s.Equals("computername"));
+
+            Action action = () => config.ConnectionFactory.GetConnection();
+
+            action.Should().Throw<InvalidOperationException>();
+
+            Console.WriteLine("all is well");
+            Console.ReadLine();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static string GetServerName(this LdapConnection connection)
+        {
+            return ((LdapDirectoryIdentifier) connection.Directory).Servers[0];
+        }
+    }
+}
